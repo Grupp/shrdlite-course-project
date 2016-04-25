99c72f5e92230dbda024142545b59e9b7c4682e8
Ekengren <johannes.o.andersson@gmail.com>
2016-04-25 19:04:57 +0200

    First implementation of aStarSearch<Node> in Graph.ts

--- Graph.ts
+++ Graph.ts
@@ -12,25 +12,25 @@
 
 /** An edge in a graph. */
 class Edge<Node> {
-    from : Node;
-    to   : Node;
-    cost : number;
+    from: Node;
+    to: Node;
+    cost: number;
 }
 
 /** A directed graph. */
 interface Graph<Node> {
     /** Computes the edges that leave from a node. */
-    outgoingEdges(node : Node) : Edge<Node>[];
+    outgoingEdges(node: Node): Edge<Node>[];
     /** A function that compares nodes. */
-    compareNodes : collections.ICompareFunction<Node>;
+    compareNodes: collections.ICompareFunction<Node>;
 }
 
 /** Type that reports the result of a search. */
 class SearchResult<Node> {
     /** The path (sequence of Nodes) found by the search algorithm. */
-    path : Node[];
+    path: Node[];
     /** The total cost of the path. */
-    cost : number;
+    cost: number;
 }
 
 /**
@@ -48,26 +48,78 @@ class SearchResult<Node> {
 * @param timeout Maximum time to spend performing A\* search.
 * @returns A search result, which contains the path from `start` to a node satisfying `goal` and the cost of this path.
 */
-function aStarSearch<Node> (
-    graph : Graph<Node>,
-    start : Node,
-    goal : (n:Node) => boolean,
-    heuristics : (n:Node) => number,
-    timeout : number
-) : SearchResult<Node> {
-    // A dummy search result: it just picks the first possible neighbour
-    var result : SearchResult<Node> = {
+function aStarSearch<Node>(
+    graph: Graph<Node>,
+    start: Node,
+    goal: (n: Node) => boolean,
+    heuristics: (n: Node) => number,
+    timeout: number
+): SearchResult<Node> {
+
+    let result: SearchResult<Node> = {
         path: [start],
         cost: 0
     };
-    while (result.path.length < 3) {
-        var edge : Edge<Node> = graph.outgoingEdges(start) [0];
-        if (! edge) break;
-        start = edge.to;
-        result.path.push(start);
-        result.cost += edge.cost;
+
+    let openSet: collections.PriorityQueue<Node> = new collections.PriorityQueue<Node>((a, b) => {
+        let fa: number = fScores.getValue(a);
+        let fb: number = fScores.getValue(b);
+        if (fa < fb) return -1;
+        if (fa == fb) return 0;
+        if (fa > fb) return 1;
+    });
+    openSet.enqueue(start);
+
+    let closedSet: collections.Set<Node> = new collections.Set<Node>();
+
+    let gScores: collections.Dictionary<Node, number> = new collections.Dictionary<Node, number>();
+    let fScores: collections.Dictionary<Node, number> = new collections.Dictionary<Node, number>();
+
+    let cameFrom: collections.Dictionary<Node, Node> = new collections.Dictionary<Node, Node>();
+
+
+    while (!openSet.isEmpty) {
+        let current: Node = openSet.dequeue();
+        if (goal(current)) {
+            result.path = backtrack(cameFrom, current, start);
+            result.cost = gScores.getValue(current);
+            return result;
+        }
+
+        closedSet.add(current);
+        graph.outgoingEdges(current).forEach(edge => {
+            if (closedSet.contains(edge.to)) return; //continue;
+
+            let tempgScore: number = gScores.getValue(current) + edge.cost;
+            if (!openSet.contains(edge.to)) openSet.enqueue(edge.to)
+            else if (tempgScore >= gScores.getValue(edge.to)) return;
+
+            cameFrom.setValue(edge.to, current);
+            gScores.setValue(edge.to, tempgScore);
+            fScores.setValue(edge.to, tempgScore + heuristics(edge.to));
+        });
     }
+
+    //Failure: timeout or no path    
     return result;
+}
+
+function backtrack<Node>(
+    cameFrom: collections.Dictionary<Node, Node>,
+    goal: Node,
+    start: Node
+): Node[] {
+
+    let path: Node[] = [goal];
+
+    let current: Node = goal;
+    while (current != start) {
+        current = cameFrom.getValue(current);
+        path.unshift(current);
+    }
+    path.unshift(start);
+
+    return path;
 }
 
 
@@ -75,65 +127,65 @@ function aStarSearch<Node> (
 // here is an example graph
 
 interface Coordinate {
-    x : number;
-    y : number;
+    x: number;
+    y: number;
 }
 
 
 class GridNode {
     constructor(
-        public pos : Coordinate
-    ) {}
+        public pos: Coordinate
+    ) { }
 
-    add(delta : Coordinate) : GridNode {
+    add(delta: Coordinate): GridNode {
         return new GridNode({
             x: this.pos.x + delta.x,
             y: this.pos.y + delta.y
         });
     }
 
-    compareTo(other : GridNode) : number {
+    compareTo(other: GridNode): number {
         return (this.pos.x - other.pos.x) || (this.pos.y - other.pos.y);
     }
 
-    toString() : string {
+    toString(): string {
         return "(" + this.pos.x + "," + this.pos.y + ")";
     }
 }
 
 /** Example Graph. */
 class GridGraph implements Graph<GridNode> {
-    private walls : collections.Set<GridNode>;
+    private walls: collections.Set<GridNode>;
 
     constructor(
-        public size : Coordinate,
-        obstacles : Coordinate[]
+        public size: Coordinate,
+        obstacles: Coordinate[]
     ) {
         this.walls = new collections.Set<GridNode>();
         for (var pos of obstacles) {
             this.walls.add(new GridNode(pos));
         }
         for (var x = -1; x <= size.x; x++) {
-            this.walls.add(new GridNode({x:x, y:-1}));
-            this.walls.add(new GridNode({x:x, y:size.y}));
+            this.walls.add(new GridNode({ x: x, y: -1 }));
+            this.walls.add(new GridNode({ x: x, y: size.y }));
         }
         for (var y = -1; y <= size.y; y++) {
-            this.walls.add(new GridNode({x:-1, y:y}));
-            this.walls.add(new GridNode({x:size.x, y:y}));
+            this.walls.add(new GridNode({ x: -1, y: y }));
+            this.walls.add(new GridNode({ x: size.x, y: y }));
         }
     }
 
-    outgoingEdges(node : GridNode) : Edge<GridNode>[] {
-        var outgoing : Edge<GridNode>[] = [];
+    outgoingEdges(node: GridNode): Edge<GridNode>[] {
+        var outgoing: Edge<GridNode>[] = [];
         for (var dx = -1; dx <= 1; dx++) {
             for (var dy = -1; dy <= 1; dy++) {
-                if (! (dx == 0 && dy == 0)) {
-                    var next = node.add({x:dx, y:dy});
-                    if (! this.walls.contains(next)) {
+                if (!(dx == 0 && dy == 0)) {
+                    var next = node.add({ x: dx, y: dy });
+                    if (!this.walls.contains(next)) {
                         outgoing.push({
                             from: node,
                             to: next,
-                            cost: Math.sqrt(dx*dx + dy*dy)
+                            cost: Math.sqrt(dx * dx + dy * dy)
                         });
                     }
                 }
@@ -142,18 +194,18 @@ class GridGraph implements Graph<GridNode> {
         return outgoing;
     }
 
-    compareNodes(a : GridNode, b : GridNode) : number {
+    compareNodes(a: GridNode, b: GridNode): number {
         return a.compareTo(b);
     }
 
-    toString() : string {
+    toString(): string {
         var borderRow = "+" + new Array(this.size.x + 1).join("--+");
         var betweenRow = "+" + new Array(this.size.x + 1).join("  +");
         var str = "\n" + borderRow + "\n";
-        for (var y = this.size.y-1; y >= 0; y--) {
+        for (var y = this.size.y - 1; y >= 0; y--) {
             str += "|";
             for (var x = 0; x < this.size.x; x++) {
-                str += this.walls.contains(new GridNode({x:x,y:y})) ? "## " : "   ";
+                str += this.walls.contains(new GridNode({ x: x, y: y })) ? "## " : "   ";
             }
             str += "|\n";
             if (y > 0) str += betweenRow + "\n";
