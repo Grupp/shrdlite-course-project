6be8269aa1faf6744466136e98ac922e69b11081
Johannes <johannes.o.andersson@gmail.com>
2016-05-27 09:29:07 +0200

    Fixed bug in Interpreter where all relations was not handled correctly

--- Interpreter.ts
+++ Interpreter.ts
@@ -36,16 +36,15 @@ module Interpreter {
     * @param currentState The current state of the world.
     * @returns Augments ParseResult with a list of interpretations. Each interpretation is represented by a list of Literals.
     */
-    export function interpret(parses : Parser.ParseResult[], currentState : WorldState) : InterpretationResult[] {
-        var errors : Error[] = [];
-        var interpretations : InterpretationResult[] = [];
+    export function interpret(parses: Parser.ParseResult[], currentState: WorldState): InterpretationResult[] {
+        var errors: Error[] = [];
+        var interpretations: InterpretationResult[] = [];
         parses.forEach((parseresult) => {
             try {
                 var result: InterpretationResult = <InterpretationResult>parseresult;
-                var intp = interpretCommand(result.parse, currentState);
-                if (intp != null) { }
-
                 result.interpretation = interpretCommand(result.parse, currentState);
+                //console.log('\n!!!!!!!!!!!\n' + stringifyParse(parseresult));
+                //console.log(stringify(result) + '\n????????????\n');
                 interpretations.push(result);
             } catch (err) {
                 errors.push(err);
@@ -77,12 +76,12 @@ module Interpreter {
          * literal {polarity: false, relation: "ontop", args:
          * ["a","b"]}.
          */
-        polarity : boolean;
+        polarity: boolean;
         /** The name of the relation in question. */
-        relation : string;
+        relation: string;
         /** The arguments to the relation. Usually these will be either objects
          * or special strings such as "floor" or "floor-N" (where N is a column) */
-        args : string[];
+        args: string[];
     }
 
     export function stringify(result: InterpretationResult): string {
@@ -96,6 +95,21 @@ module Interpreter {
         return (lit.polarity ? "" : "-") + lit.relation + "(" + lit.args.join(",") + ")";
     }
 
+    function stringifyParse(parse: Parser.ParseResult): string {
+        let s: string[] = [];
+        let j = JSON.stringify(parse.parse).split(/({|})/g);
+        let tabs = 1;
+        j.forEach(sj => {
+            if (sj.lastIndexOf('}') != -1)
+                tabs--;
+            s.push(((new Array(tabs).join(" " + tabs)) + sj));
+            if (sj.lastIndexOf('{') != -1)
+                tabs++;
+        });
+
+        return s.join('\n');
+    }
+
     //////////////////////////////////////////////////////////////////////
     // private functions
     /**
@@ -107,6 +121,7 @@ module Interpreter {
      * @throws An error when no valid interpretations can be found
      */
     function interpretCommand(cmd: Parser.Command, state: WorldState): DNFFormula {
+
         let worldObjs = new collections.Dictionary<string, WorldObject>();
 
         let emptyColumns = 0;
@@ -129,27 +144,33 @@ module Interpreter {
 
         switch (cmd.command) {
             case 'take':
+                if (state.holding != null) throw "Arm is holding already holding something";
                 actionObjects = findMatchingObjects(cmd.entity, worldObjs, true);
                 actionObjects.forEach(obj => {
+                    //console.log(state.stacks[worldObjs.getValue(obj).column].length);
+                    //console.log(worldObjs.getValue(obj).row + 1);
+                    //only free objects
+                    //if (state.stacks[worldObjs.getValue(obj).column].length == worldObjs.getValue(obj).row + 1)
                     interpretation.push([{ polarity: true, relation: "holding", args: [obj] }])
                 });
-                return interpretation;
+                break;
             case 'put':
-                if (!state.holding) throw "Arm is holding nothing";
+                if (state.holding == null) throw "Arm is holding nothing";
                 let holding: WorldObject = new WorldObject(state.objects[state.holding], -1, -1, state.holding);
                 targetObjects = findMatchingObjects(cmd.location.entity, worldObjs, false);
                 targetObjects.forEach(tObj => {
-                    if (state.holding != tObj && obeyLaws(holding, worldObjs.getValue(tObj), cmd.location.relation)) {
+                    if (state.holding != tObj && obeyLaws(holding.obj, worldObjs.getValue(tObj).obj, cmd.location.relation)) {
                         interpretation.push([{ polarity: true, relation: cmd.location.relation, args: [state.holding, tObj] }])
                     }
                 });
                 break;
             case 'move':
+                if (state.holding != null) throw "Arm is holding already holding something";
                 actionObjects = findMatchingObjects(cmd.entity, worldObjs, true);
                 targetObjects = findMatchingObjects(cmd.location.entity, worldObjs, false);
                 actionObjects.forEach(aObj => {
                     targetObjects.forEach(tObj => {
-                        if (aObj != tObj && obeyLaws(worldObjs.getValue(aObj), worldObjs.getValue(tObj), cmd.location.relation)) {
+                        if (aObj != tObj && obeyLaws(worldObjs.getValue(aObj).obj, worldObjs.getValue(tObj).obj, cmd.location.relation)) {
                             interpretation.push([{ polarity: true, relation: cmd.location.relation, args: [aObj, tObj] }])
                         }
                     });
@@ -159,7 +180,6 @@ module Interpreter {
 
                 throw "Not implemented.";
         }
-
         if (interpretation.length == 0)
             throw "No interpetation!";
 
@@ -187,40 +207,49 @@ module Interpreter {
     ): string[] {
 
         let matches: string[] = [];
-        let entityObj = entity.object.location ? entity.object.object : entity.object;
-        let hasRelations: boolean = entity.object.location != null;
-
-        if (entityObj.form == "floor") {
-            return ["floor"];
+        let entityObj = entity.object;
+        let locations: Parser.Location[] = [];
+        while (entityObj.form == undefined) {
+            if (entityObj.location != null) {
+                locations.push(entityObj.location);
+            }
+            entityObj = entityObj.object;
         }
 
+        //let entityObj = entity.object.location ? entity.object.object : entity.object;
+
         worldObjects.forEach((key, worldObj) => {
-            //
+
             let isMatch: boolean = true;
             // Cannot pickup floor
-            if (action && key == "floor") {
+            if (key == "floor" && (action || entityObj.form == "anyform")) {
                 isMatch = false;
             }
 
             if (isMatch && entityObj.form != "anyform" && !strComp(entityObj.form, worldObj.obj.form)) {
                 isMatch = false;
             }
+
             if (isMatch && !strComp(entityObj.color, worldObj.obj.color)) {
                 isMatch = false;
             }
+
             if (isMatch && !strComp(entityObj.size, worldObj.obj.size)) {
                 isMatch = false;
             }
 
-            if (hasRelations && isMatch) {
-                let relativeObjs =
-                    findMatchingObjects(entity.object.location.entity, worldObjects, false);
-                let obeys: boolean = false;
-                relativeObjs.forEach(relativeObj => {
-                    if (obeyRelation(worldObj, worldObjects.getValue(relativeObj), entity.object.location.relation))
-                        obeys = true;
+            if (isMatch && locations.length != 0) {
+                locations.forEach(location => {
+                    let relativeObjs =
+                        findMatchingObjects(location.entity, worldObjects, false);
+                    let obeys: boolean = false;
+                    relativeObjs.forEach(relativeObj => {
+                        if (obeyRelation(worldObj, worldObjects.getValue(relativeObj), location.relation))
+                            obeys = true;
+                    });
+                    if (!obeys)
+                        isMatch = false;
                 });
-                isMatch = obeys;
             }
 
             if (isMatch) {
@@ -280,45 +309,46 @@ module Interpreter {
         return true;
     }
 
-    function obeyLaws(
-        mainObject: WorldObject,
-        relativeObject: WorldObject,
+    export function obeyLaws(
+        mainObject: ObjectDefinition,
+        relativeObject: ObjectDefinition,
         relation: string
     ): boolean {
         if (relation == "ontop" || relation == "inside") {
             // The floor can support at most N objects (beside each other).
-            if(relativeObject.obj.form == "floor" && relativeObject.column <= 0) // floor's column number are the number of free columns
-                return false;
+            //if (relativeObject.form == "floor" && relativeObject.column <= 0) // floor's column number are the number of free columns
+            //return false;
             // Small objects cannot support large objects.
-            if (mainObject.obj.size == "large" && relativeObject.obj.size == "small")
+            if (mainObject.size == "large" && relativeObject.size == "small")
                 return false;
             // Balls cannot support anything.
-            if (relativeObject.obj.form == "ball")
+            if (relativeObject.form == "ball")
                 return false;
             // Balls must be in boxes or on the floor, otherwise they roll away.
-            if (mainObject.obj.form == "ball" &&
-                relativeObject.obj.form != "box" &&
-                relativeObject.obj.form != "floor")
+            if (mainObject.form == "ball" &&
+                relativeObject.form != "box" &&
+                relativeObject.form != "floor")
                 return false;
             // Objects are “inside” boxes, but “ontop” of other objects.
-            if (relativeObject.obj.form == "box" && relation != "inside")
+            if (relativeObject.form == "box" && relation != "inside")
                 return false;
-            if (relativeObject.obj.form != "box" && relation != "ontop")
+            if (relativeObject.form != "box" && relation != "ontop")
                 return false;
             // Boxes cannot contain pyramids, planks or boxes of the same size.
-            if (mainObject.obj.form == "box" &&
-                (relativeObject.obj.form == "pyramid" || relativeObject.obj.form == "plank" || relativeObject.obj.form == "box") &&
-                mainObject.obj.size == relativeObject.obj.size)
+            if (mainObject.form == "box" &&
+                (relativeObject.form == "pyramid" || relativeObject.form == "plank" || relativeObject.form == "box") &&
+                mainObject.size == relativeObject.size)
                 return false;
             // Small boxes cannot be supported by small bricks or pyramids.
-            if (mainObject.obj.size == "small" && relativeObject.obj.size == "small" && mainObject.obj.form == "box" &&
-                (relativeObject.obj.form == "brick" || relativeObject.obj.form == "pyramid" || relativeObject.obj.form == "box"))
+            if (mainObject.size == "small" && relativeObject.size == "small" && mainObject.form == "box" &&
+                (relativeObject.form == "brick" || relativeObject.form == "pyramid" || relativeObject.form == "box"))
                 return false;
             // Large boxes cannot be supported by large pyramids.
-            if (mainObject.obj.size == "large" && mainObject.obj.form == "box" &&
-                relativeObject.obj.size == "large" && relativeObject.obj.form == "box")
+            if (mainObject.size == "large" && mainObject.form == "box" &&
+                relativeObject.size == "large" && relativeObject.form == "box")
                 return false;
         }
+        //throw "True";
         return true;
     }
 
